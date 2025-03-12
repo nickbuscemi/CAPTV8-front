8b100eb2ef72b86499ec85184fba5c9462b38fbd
Nicholas Buscemi <nicholasbuscemi@Nicholass-MBP.fios-router.home>
2025-03-12 14:42:48 -0400

    second commit

---
+++ MainFile/src/components/logo/LogoItem.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+//import LogoWhite2 from "../../../public/assets/imgs/logo/site-logo-white-2.png";
+//import LogoBlack from "../../../public/assets/imgs/logo/logo-black.png";
+import captv8_transparent_light from "../../../public/assets/logos/captv8_transparent_light.png";
+import captv8_transparent_dark from "../../../public/assets/logos/captv8_transparent_dark.png";
+import Image from "next/image";
+
+export default function LogoItem() {
+  return (
+    <>
+      <div className="header__logo-2">
+        <Link href={"/digital-marketing"} className="logo-dark">
+          <Image
+            priority
+            width={150}
+            height={45}
+            src={captv8_transparent_dark}
+            alt="Site Logo"
+          />
+        </Link>
+        <Link href={"/digital-marketing"} className="logo-light">
+          <Image
+            priority
+            width={150}
+            height={45}
+            src={captv8_transparent_light}
+            alt="Site Logo"
+          />
+        </Link>
+      </div>
+    </>
+  );
+}
